c7897d10f22bf41b77aaf9cf1fa571adc1379c74
Black-at <levraizedrox30@gmail.com>
2025-03-07 19:23:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-// On attend que la page soit complètement chargée
-window.onload = function() {
-    // Attendre 3 secondes avant de cacher l'écran de chargement
-    setTimeout(function() {
-      // Masquer l'élément de chargement
-      document.querySelector('.loading').style.display = 'none';
-      // Afficher le contenu principal
-      document.querySelector('.main-content').style.display = 'block';
-    }, 3000); // 3000 millisecondes = 3 secondes
+document.addEventListener("DOMContentLoaded", () => {
+  const loadingScreen = document.querySelector(".loading");
+  const mainContent = document.querySelector(".main-content");
+
+  // 
+  //
+  window.stopLoading = function() {
+    loadingScreen.classList.add("loaded");
+    mainContent.classList.add("loaded");
   };
-  
+});
+
